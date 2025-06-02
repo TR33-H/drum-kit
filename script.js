f5996efe9b14baf79d524ecd638fe157911703ae
TR33-H <demetriehayes@proton.me>
2025-06-02 07:23:20 -0500

    Allows same sound to be played back to back.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.addEventListener('keydown', function(e){
     const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if(!audio) return;
+    audio.currentTime=0; //starts from beginning
     audio.play();
     console.log(audio);
 });
