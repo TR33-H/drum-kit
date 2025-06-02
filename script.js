23d0ef64dafc1e0a0d4c3ce616cff7ea7d14ad90
TR33-H <demetriehayes@proton.me>
2025-06-02 07:37:24 -0500

    Added basic styling. Also replaced second instance of "A" with "S".

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.addEventListener('keydown', function(e){
     const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return;
     audio.currentTime=0; //starts from beginning
     audio.play();
-    console.log(audio);
+    console.log(key);
 });
