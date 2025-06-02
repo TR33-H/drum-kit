116bb9d26f02875ff8140581ee7fc806f8f2a66d
TR33-H <demetriehayes@proton.me>
2025-06-02 07:22:20 -0500

    Added basic reading of key inputs to play audio. However, a delay is happening if the same key is hit one too many times in a short duration.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 window.addEventListener('keydown', function(e){
-    console.log(e);
+    const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.play();
+    console.log(audio);
 });
